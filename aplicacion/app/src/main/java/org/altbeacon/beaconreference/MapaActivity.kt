4717843abfbbcf7eca492067e6d895fe5b48654c
diegarlin <garcialinaresdiego@gmail.com>
2024-02-23 14:06:16 +0100

    Cambios en estilos y actividades.

--- aplicacion/app/src/main/java/org/altbeacon/beaconreference/MapaActivity.kt
+++ aplicacion/app/src/main/java/org/altbeacon/beaconreference/MapaActivity.kt
@@ -1,6 +1,7 @@
 package org.altbeacon.beaconreference
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
@@ -10,7 +11,7 @@ import android.webkit.WebViewClient
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MapaActivity : AppCompatActivity() {
+class MapaActivity : Activity() {
 
         @SuppressLint("SetJavaScriptEnabled")
         override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +19,7 @@ class MapaActivity : AppCompatActivity() {
         setContentView(R.layout.mapa_activity)
 
         val webView: WebView = findViewById(R.id.webView)
-        val backButton: Button = findViewById(R.id.backButton)
+
 
         // Configuración del WebView
         val webSettings: WebSettings = webView.settings
@@ -29,13 +30,11 @@ class MapaActivity : AppCompatActivity() {
         override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
         view?.loadUrl(request?.url.toString())
         return true
-        }
+                }
         }
 
         webView.loadUrl("https://ap.cs.ucy.ac.cy/viewer/?buid=building_3f91a8fd-ca7b-4d7e-bc04-626e2623d229_1704557723480&floor=0")
 
-        backButton.setOnClickListener {
-        onBackPressed() // Volver a la actividad anterior (MainActivity)
-        }
-        }
-        }
+
+    }
+}
