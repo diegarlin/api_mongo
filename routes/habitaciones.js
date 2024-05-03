bac38f4d22d3468c1be75561a6afdc4be54f42e2
diegarlin <garcialinaresdiego@gmail.com>
2024-05-03 13:19:30 +0200

    Ahora tengo el numero de entradas y las personas dentro separadas, además el metodo que va por letra si no le opngo letra es el de todas las habitaciones

--- routes/habitaciones.js
+++ routes/habitaciones.js
@@ -3,80 +3,40 @@ const router = express.Router();
 const Registro = require('../models/registro');
 const moment = require('moment');
 
-// Obtener el número de personas en cada habitación
-router.get('/personas_todas_habitaciones', async (req, res) => {
+
+// Obtener el número de personas que ha pasado/entrado por cada habitación en la historia que comienza con una letra específica
+router.get('/entradas_por_habitacion', async (req, res) => {
     try {
-        // Obtener todos los registros de entrada que no tienen un registro de salida correspondiente
-        const registrosActivos = await Registro.find({ tipo: 'entrada' }).lean();
+        // Obtiene la letra desde req.query
+        const { letra } = req.query;
 
-        // Crear un objeto para almacenar el recuento de personas en cada habitación
-        const habitaciones = {};
+        // Crea una expresión regular para buscar habitaciones que comiencen con el parámetro proporcionado
+        // Si no se proporciona ninguna letra, la expresión regular coincidirá con cualquier habitación
+        const roomRegex = letra ? new RegExp(`^${letra}`, 'i') : /.*/;
 
-        // Recorrer los registros activos y actualizar el recuento para cada habitación
-        registrosActivos.forEach(registro => {
-            const habitacion = registro.habitacion;
-            if (habitaciones[habitacion]) {
-                habitaciones[habitacion]++;
-            } else {
-                habitaciones[habitacion] = 1;
-            }
-        });
+        // Obtén una lista de todas las habitaciones que coinciden con la expresión regular
+        const rooms = await Registro.find({ habitacion: { $regex: roomRegex } }).distinct('habitacion');
 
-        // Convertir el objeto habitaciones en una lista de objetos
-        const listaHabitaciones = Object.keys(habitaciones).map(habitacion => ({
-            habitacion: habitacion,
-            numPersonas: habitaciones[habitacion]
+        // Para cada habitación, cuenta el número de 'entrada' registros
+        const counts = await Promise.all(rooms.map(async (room) => {
+            const entradaCount = await Registro.countDocuments({ habitacion: room, tipo: 'entrada' });
+
+            // Devuelve un objeto con el nombre de la habitación y el número de 'entrada' registros
+            return {
+                habitacion: room,
+                numEntradas: entradaCount
+            };
         }));
 
-        // Si no hay ninguna habitación con personas, devolver un error
-        if (listaHabitaciones.length === 0) {
-            return res.status(400).json({ msg: "No hay ninguna habitación con personas" });
-        }
-
-        res.json(listaHabitaciones);
+        // Devuelve la lista de conteos
+        res.json(counts);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
-router.get('/personas_por_habitacion/:letra', async (req, res) => {
-    try {
-        const letra = req.params.letra.toUpperCase();
-
-        // Obtener todos los registros
-        const todosLosRegistros = await Registro.find();
-
-        const habitaciones = {};
-
-        // Inicializar el recuento para todas las habitaciones que comienzan con la letra especificada
-        todosLosRegistros.forEach(registro => {
-            const habitacion = registro.habitacion;
-            if (habitacion.toUpperCase().startsWith(letra)) {
-                habitaciones[habitacion] = 0;
-            }
-        });
-
-        // Recorrer todos los registros y actualizar el recuento para cada habitación de tipo 'entrada'
-        todosLosRegistros.forEach(registro => {
-            const habitacion = registro.habitacion;
-            if (registro.tipo === 'entrada' && habitacion.toUpperCase().startsWith(letra)) {
-                habitaciones[habitacion]++;
-            }
-        });
-
-        // Convertir el objeto habitaciones en una lista de objetos
-        const listaHabitaciones = Object.keys(habitaciones).map(habitacion => ({
-            habitacion: habitacion,
-            numPersonas: habitaciones[habitacion]
-        }));
-
-        res.json(listaHabitaciones);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-router.get('/personas_por_habitacion_fecha', async (req, res) => {
+// Obtener el número de personas que han pasado/entrado por cada habitación entre fecha inicio y fecha fin
+router.get('/entradas_por_habitacion_fecha', async (req, res) => {
     try {
         const { habitacion, fechaInicio, fechaFin } = req.query;
 
@@ -126,4 +86,85 @@ router.get('/personas_por_habitacion_fecha', async (req, res) => {
     }
 });
 
+router.get('/personas_actual_habitaciones', async (req, res) => {
+    try {
+        // Crea una expresión regular para buscar habitaciones que comiencen con el parámetro proporcionado
+        const { letra } = req.query;
+
+        // Crea una expresión regular para buscar habitaciones que comiencen con el parámetro proporcionado
+        // Si no se proporciona ninguna letra, la expresión regular coincidirá con cualquier habitación
+        const roomRegex = letra ? new RegExp(`^${letra}`, 'i') : /.*/;
+
+        // Obtén una lista de todas las habitaciones que coinciden con la expresión regular
+        const rooms = await Registro.find({ habitacion: { $regex: roomRegex } }).distinct('habitacion');
+
+        // Para cada habitación, cuenta el número de personas en la habitación
+        const counts = await Promise.all(rooms.map(async (room) => {
+            const entradaRegistros = await Registro.find({ habitacion: room, tipo: 'entrada' }).lean();
+            const peopleInRoom = await Promise.all(entradaRegistros.map(async (entrada) => {
+                 // Para cada registro de 'entrada', busca un registro de 'salida' correspondiente
+                // Un registro de 'salida' correspondiente es uno que tiene el mismo deviceID y habitación,
+                // y una fechaHora mayor que la del registro de 'entrada'
+
+                const salidaRegistro = await Registro.findOne({ habitacion: room, tipo: 'salida', deviceID: entrada.deviceID, fechaHora: { $gt: entrada.fechaHora } }).lean();
+
+                // Si no hay un registro de 'salida' correspondiente, la persona todavía está en la habitación
+                // Por lo tanto, devuelve true si no hay un registro de 'salida' correspondiente, y false de lo contrario
+                return !salidaRegistro;
+            }));
+
+            // Devuelve un objeto con el nombre de la habitación y el número de personas en esa habitación
+            return {
+                habitacion: room,
+                numPersonas: peopleInRoom.filter(Boolean).length
+            };
+        }));
+
+        // Devuelve la lista de conteos
+        res.json(counts);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+router.get('/personas_actual_fecha', async (req, res) => {
+    try {
+        // Obtiene la fecha y la habitación desde req.query
+        const { fecha, habitacion } = req.query;
+
+        // Crea una expresión regular para buscar habitaciones que comiencen con el parámetro proporcionado
+        // Si no se proporciona ninguna habitacion, la expresión regular coincidirá con cualquier habitación
+        const roomRegex = habitacion ? new RegExp(`^${habitacion}`, 'i') : /.*/;
+
+        // Convierte la fecha proporcionada a un objeto Date
+        const fechaDate = moment.utc(fecha, "YYYY-MM-DD HH:mm").utcOffset("+02:00").toDate();
+
+        // Obtén una lista de todas las habitaciones que coinciden con la expresión regular
+        const rooms = await Registro.find({ habitacion: { $regex: roomRegex } }).distinct('habitacion');
+
+        // Para cada habitación, cuenta el número de personas en la habitación en la fecha proporcionada
+        const counts = await Promise.all(rooms.map(async (room) => {
+            const entradaRegistros = await Registro.find({ habitacion: room, tipo: 'entrada', fechaHora: { $lte: fechaDate } }).lean();
+            const peopleInRoom = await Promise.all(entradaRegistros.map(async (entrada) => {
+                const salidaRegistro = await Registro.findOne({ habitacion: room, tipo: 'salida', deviceID: entrada.deviceID, fechaHora: { $gt: entrada.fechaHora, $lte: fechaDate } }).lean();
+                return !salidaRegistro;
+            }));
+
+            // Devuelve un objeto con el nombre de la habitación y el número de personas en esa habitación
+            return {
+                habitacion: room,
+                numPersonas: peopleInRoom.filter(Boolean).length
+            };
+        }));
+
+        // Devuelve la lista de conteos
+        res.json(counts);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 module.exports = router;
+
+
